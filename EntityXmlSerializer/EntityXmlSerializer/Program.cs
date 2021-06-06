66d16281985f24cd67a7574b8d8493d938a42cf7
Krzysztof Borowiecki <krzysztof.borowiecki58@gmail.com>
2021-06-06 16:11:47 +0200

    Init project

---
+++ EntityXmlSerializer/EntityXmlSerializer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityXmlSerializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

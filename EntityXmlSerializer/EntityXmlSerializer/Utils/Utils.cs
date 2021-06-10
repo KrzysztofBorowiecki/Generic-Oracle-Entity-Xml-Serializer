11c1e58fe62de1b6c2ef5c8385a0ccdb0c2f8f0f
Krzysztof Borowiecki <krzysztof.borowiecki58@gmail.com>
2021-06-10 16:55:37 +0200

    add Utils class

---
+++ EntityXmlSerializer/EntityXmlSerializer/Utils/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace EntityXmlSerializer.Utils
+{
+    public static class Utils
+    {
+        public static void Exit(string message = null, int exitCode = 0)
+        {
+            if (message != null)
+            {
+                Console.WriteLine(message);
+            }
+
+            Environment.Exit(exitCode);
+        }
+
+        public static DirectoryInfo GetDirectoryInfo(string path)
+        {
+            var dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+
+            return dirInfo;
+        }
+    }
+}
